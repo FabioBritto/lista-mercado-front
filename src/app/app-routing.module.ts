31776daf5b9f3fe0b6d5355dfcfce1779f71ee47
FabioBritto <fabio.tritono@gmail.com>
2025-02-24 19:57:44 -0300

    Criado front inicial com btn de adicionar lista

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListasComponent } from './componentes/listas/listas.component';
+import { DetalhelistaComponent } from './componentes/detalhelista/detalhelista.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'', component: ListasComponent},
+{path:'detalhe', component : DetalhelistaComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
+//Imports do Angular
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
 
+//Imports meus
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListasComponent } from './componentes/listas/listas.component';
+import { DetalhelistaComponent } from './componentes/detalhelista/detalhelista.component';
 
+
+//Os DECLARATIONS são os componentes criados por mim
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListasComponent,
+    DetalhelistaComponent
   ],
-  imports: [
+  //Os IMPORTS são os componentes que eu usarei do ANGULAR
+  imports: [ 
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule, //Preciso do componente de Requisições HTTP
+    FormsModule // Preciso do componente de manipulação de Formulários
   ],
   providers: [],
   bootstrap: [AppComponent]
